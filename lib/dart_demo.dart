3b2b5b845b209f09dabc5fd1e947f0ff575c46f4
MuFeng-T <hanlin_bj@163.com>
2019-08-24 15:17:42 +0800

    Dart基础

---
+++ lib/dart_demo.dart
@@ -0,0 +1,24 @@
+main(){
+//  Object name;
+//  name = "MuFeng";
+//
+//  dynamic name1;
+//  name1 = "MuFeng";
+//
+////  print(name.length);
+//  print(name1.length);
+
+//  int count;
+//  bool isShow;
+//  String name;
+//  double score;
+//
+//  print("count = $count");
+//  print("isShow = $isShow");
+//  print("name = $name");
+//  print("score = $score");
+
+  final name = "a";
+  final String nickName = "1";
+
+}
